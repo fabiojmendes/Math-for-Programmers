798aa446238026a64bd932487dbf8a3998e1a736
Fabio Mendes <fabiojmendes@gmail.com>
2021-03-28 20:16:14 -0400

    Collision detection exercise

---
+++ Chapter 07/ex714.py
@@ -0,0 +1,89 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from vectors import *
+from vector_drawing import *
+
+
+def segments(obj):
+    return [(obj[i], obj[(i+1) % len(obj)]) for i in range(len(obj))]
+
+
+def standard_form(u, v):
+    x1, y1 = u
+    x2, y2 = v
+    a = y2 - y1
+    b = x1 - x2
+    c = x1*y2 - x2*y1
+    return (a, b, c)
+
+
+def intersection(s1, s2):
+    a1, b1, c1 = standard_form(*s1)
+    a2, b2, c2 = standard_form(*s2)
+    matrix = np.array(((a1, b1), (a2, b2)))
+    result = np.array((c1, c2))
+    return np.linalg.solve(matrix, result)
+
+
+def segments_intersect(s1, s2):
+    u1, u2 = s1
+    v1, v2 = s2
+    l1, l2 = distance(*s1), distance(*s2)
+    try:
+        x, y = intersection(s1, s2)
+        return (distance(u1, (x, y)) <= l1 and
+                distance(u2, (x, y)) <= l1 and
+                distance(v1, (x, y)) <= l2 and
+                distance(v2, (x, y)) <= l2)
+    except np.linalg.linalg.LinAlgError:
+        return False
+
+
+# spaceship = [
+#     (0.5, 0),
+#     (-0.25, 0.25),
+#     (-0.25, -0.25)
+# ]
+
+spaceship = [
+    (0.5, 0),
+    (-0.25, 0.25),
+    (-0.25, -0.25)
+]
+spaceship = [rotate2d(pi/2, v) for v in spaceship]
+spaceship = translate((2, 3), spaceship)
+
+
+asteroid = [
+    (2, 3),
+    (4, 2),
+    (6, 2),
+    (7, 4),
+    (6, 6),
+    (4, 6),
+    (2, 7),
+    (1, 5),
+]
+
+intersections = [[seg1, seg2]
+                 for seg1 in segments(asteroid)
+                 for seg2 in segments(spaceship)
+                 if segments_intersect(seg1, seg2)]
+
+# intersections = [item for inter in intersections
+#                  for item in inter]
+
+
+# print("Asteroid segments", segments)
+
+print("Intersections", intersections)
+
+draw(
+    Points(*spaceship),
+    Polygon(*spaceship, color=blue, fill=blue),
+
+    Points(*asteroid),
+    Polygon(*asteroid, color=green, fill=green),
+    *[Segment(*seg, color=red) for item in intersections for seg in item],
+)
